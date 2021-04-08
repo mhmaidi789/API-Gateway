7120f8a7afa04d4eda2e5c15f32048b007a73e38
“mhmaidi789” <mo@Mahmouds-MBP.local>
2021-04-08 13:43:31 -0500

    Added effects when clicking result buttons

--- client/App/Components/SearchResults.jsx
+++ client/App/Components/SearchResults.jsx
@@ -14,10 +14,10 @@ class SearchResults extends React.Component{
         return(
             <Fade style = {{display: 'flex', flex: 1, flexDirection: 'column' }} in = {true} timeout = {this.props.timeout*1000} >
                 <div>         
-                    <button onClick = {this.props.selectButton} style = {{padding: '1em', margin: '1em', borderStyle: 'solid', border: 2, backgroundColor: '#fffeed', boxShadow: '1px 1px 1px 1px', borderRadius: '0.5em'}}>
-                        <div><strong>Book Title: </strong>{this.props.books.title}</div>
-                        <div><strong>Author: </strong>{this.props.books.author}</div>
-                        <div style = {{wordWrap: 'break-word'}}><strong>API: </strong>{this.props.books.selfLink}</div>
+                    <button className = {'resultButton'} onClick = {this.props.selectButton}>
+                        <div style = {{color: 'white'}}><strong>Book Title: </strong>{this.props.books.title}</div>
+                        <div style = {{color: 'white'}}><strong>Author: </strong>{this.props.books.author}</div>
+                        <div style = {{wordWrap: 'break-word', color: 'white'}}><strong>API: </strong>{this.props.books.selfLink}</div>
                     </button>
                 </div>
             </Fade>    
