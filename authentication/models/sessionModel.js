b07493c83aea1c12f506001b99db2ef7ec1fa4ce
zedsauvage <21992503+zedsauvage@users.noreply.github.com>
2021-04-07 19:16:21 -0700

    connect Mongo DB for use with session/user info

--- authentication/models/sessionModel.js
+++ authentication/models/sessionModel.js
@@ -10,7 +10,7 @@ const Schema = mongoose.Schema;
 */
 const sessionSchema = new Schema({
   cookieId: { type: String, required: true, unique: true },
-  createdAt: { type: Date, expires: 30, default: Date.now }
+  createdAt: { type: Date, default: Date.now } //HSDC removed expires field
 });
 
 module.exports = mongoose.model('Session', sessionSchema);
